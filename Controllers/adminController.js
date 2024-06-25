059c1fe8e9614da2132743b16b1121ce38ebe373
surendrasuresh1999 <k.surendraparla1999@gmail.com>
2024-06-25 18:10:47 +0530

    feat://admin controller added

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,4 +1,4 @@
-const admin = require("../Models/adminModel");
+const adminmodel = require("../Models/adminModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -8,43 +8,12 @@ const createJwtToken = (userId) => {
   });
 };
 
-const loginAdmin = async (req, res) => {
-  //   const { _id } = req.user;
-  const { email, password } = req.body;
-  console.log(req.body);
-  try {
-    const isAdminExist = await admin.findOne({ email });
-    console.log(isAdminExist);
-    if (!isAdminExist) {
-      return res.json({
-        status: false,
-        message: "You can't access this application",
-      });
-    }
-    const match = await bcrypt.compare(password, isAdminExist.password);
-
-    if (!match) {
-      return res.json({ status: false, message: "Invalid password" });
-    }
-    // create token
-    const token = createJwtToken(user._id);
-    return res.json({
-      status: true,
-      message: "logged in successfully",
-      user,
-      token,
-    });
-  } catch (error) {
-    return res.json({ status: 404, message: error.message });
-  }
-};
-
 const signupAdmin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     // Check if a user with the provided email already exists
-    const existingUser = await admin.findOne({ email });
+    const existingUser = await adminmodel.findOne({ email });
 
     if (existingUser) {
       return res.json({ status: false, message: "email already exists" });
@@ -54,7 +23,7 @@ const signupAdmin = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     // Create a new user
-    const newUser = await admin.create({
+    const newUser = await adminmodel.create({
       email,
       password: hashedPassword,
     });
@@ -70,11 +39,43 @@ const signupAdmin = async (req, res) => {
   }
 };
 
+const loginAdmin = async (req, res) => {
+  //   const { _id } = req.user;
+  const { email, password } = req.body;
+
+  try {
+    const isAdminExist = await adminmodel.findOne({
+      email,
+    });
+    if (!isAdminExist) {
+      return res.json({
+        status: false,
+        message: "Sorry you don't have permission",
+      });
+    }
+    const match = await bcrypt.compare(password, isAdminExist.password);
+
+    if (!match) {
+      return res.json({ status: false, message: "Invalid password" });
+    }
+    // create token
+    const token = createJwtToken(isAdminExist._id);
+    return res.json({
+      status: true,
+      message: "logged in successfully",
+      isAdminExist,
+      token,
+    });
+  } catch (error) {
+    return res.json({ status: 404, message: error.message });
+  }
+};
+
 const forgotPasswordAdmin = async (req, res) => {
   const { email, password, confirmPassword } = req.body;
 
   try {
-    const user = await admin.findOne({ email });
+    const user = await adminmodel.findOne({ email });
     if (!user) {
       return res.json({ status: false, message: "User doesn't exist" });
     }
